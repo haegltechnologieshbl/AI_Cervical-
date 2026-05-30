ad91376d3cbd18a848a7a5c01fff9a81e951e1ba
Deepakgg007 <deepakg081999@gmail.com>
2026-05-30 15:26:23 +0530

    new updates

--- cervistage/settings.py
+++ cervistage/settings.py
@@ -52,10 +52,16 @@ TEMPLATES = [
 ROOT_URLCONF = "cervistage.urls"
 WSGI_APPLICATION = "cervistage.wsgi.application"
 
+import os
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DB_NAME', 'db.sqlite3'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
 
